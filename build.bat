3cd9df5ddcd4fd673fee9d951225dd3b86f29e34
dicedpineapples <alexandermartin006@gmail.com>
2022-08-01 18:31:43 -0700

    rename

--- build.bat
+++ build.bat
@@ -6,4 +6,4 @@ echo asdfasdfasdf > lock.tmp
 cl /Zi /LD fizzicks.cpp /link /NOLOGO /export:tick /incremental:no -PDB:fizzicks_%random%.pdb
 del lock.tmp
 
-cl /Zi platform.cpp /I .\include\SDL2 /link .\lib\x64\SDL2.lib .\lib\x64\SDL2main.lib /SUBSYSTEM:WINDOWS /NODEFAULTLIB:libcmtd.lib opengl32.lib /NOLOGO /incremental:no
+cl /Zi fizzicks_win32.cpp /I .\include\SDL2 /link .\lib\x64\SDL2.lib .\lib\x64\SDL2main.lib /SUBSYSTEM:WINDOWS /NODEFAULTLIB:libcmtd.lib opengl32.lib /NOLOGO /incremental:no
